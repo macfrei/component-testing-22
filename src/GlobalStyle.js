f84e8a9c68eee201241b97b02f89100f3ee2637c
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-10 07:14:25 +0100

    Update global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,18 +1,23 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
 * {
   box-sizing: border-box;
 }
+
 body {
   font-family: sans-serif;
-  font-size: 112.5%;
   margin: 0 auto;
   max-width: 600px;
 }
+
 input,
 button {
   font-size: inherit;
   padding: 5px;
+}
+
+h1, h2, h3, h4, h5, h6 {
+   margin: 0;
 }
 `;
