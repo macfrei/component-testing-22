4d60d94997d11fef643fa31d6825b6b30049edd5
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-12 11:50:17 +0100

    Add game form test

---
+++ src/components/GameForm.spec.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import GameForm from './GameForm';
+
+describe('GameForm', () => {
+  it('renders two input fields and a button', () => {
+    render(<GameForm />);
+
+    const nameOfGameInput = screen.getByLabelText(/name of game/i);
+    const playerNamesInput = screen.getByLabelText(/player names/i);
+    const submitButton = screen.getByRole('button', { name: /create/i });
+
+    expect(nameOfGameInput).toBeInTheDocument();
+    expect(playerNamesInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('renders a form with the name "Create a new game"', () => {
+    render(<GameForm />);
+    // test for accessibility
+    const form = screen.getByRole('form', { name: 'Create a new game' });
+
+    expect(form).toBeInTheDocument();
+  });
+
+  it('submits form data', () => {
+    const handleCreate = jest.fn();
+    render(<GameForm onCreateGame={handleCreate} />);
+
+    const nameOfGameInput = screen.getByLabelText(/name of game/i);
+    const playerNamesInput = screen.getByLabelText(/player names/i);
+    const submitButton = screen.getByRole('button', { name: /create/i });
+
+    userEvent.type(nameOfGameInput, 'Dodelido');
+    userEvent.type(playerNamesInput, 'Jane, John');
+    userEvent.click(submitButton);
+
+    expect(handleCreate).toHaveBeenCalledWith({
+      nameOfGame: 'Dodelido',
+      playerNames: ['Jane', 'John'],
+    });
+  });
+});
