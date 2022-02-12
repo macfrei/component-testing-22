f855b7433997a449f5cfd919d5d1cc05153be676
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-12 13:08:49 +0100

    Write test for exercise 2 of component testing

--- src/components/HistoryEntry.spec.js
+++ src/components/HistoryEntry.spec.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import HistoryEntry from './HistoryEntry';
 
 describe('HistoryEntry', () => {
-  it('renders finished game data', () => {
+  it('renders name of game and "show score" button only', () => {
     render(
       <HistoryEntry
         nameOfGame="Dodelido"
@@ -14,15 +15,32 @@ describe('HistoryEntry', () => {
     );
 
     const nameOfGame = screen.getByText(/dodelido/i);
+    const button = screen.getByRole('button', { name: /show score/i });
+
+    expect(nameOfGame).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders player names and scores after button click', () => {
+    render(
+      <HistoryEntry
+        nameOfGame="Dodelido"
+        players={[
+          { name: 'John', score: 2, id: 'xyz' },
+          { name: 'Jane', score: 1, id: 'abc' },
+        ]}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: /show score/i });
+
+    userEvent.click(button);
+
     const player1 = screen.getByText(/john/i);
     const player2 = screen.getByText(/jane/i);
     const playerScore1 = screen.getByText(/2/i);
     const playerScore2 = screen.getByText(/1/i);
 
-    // Creates interactive testing playground
-    // screen.logTestingPlaygroundURL();
-
-    expect(nameOfGame).toBeInTheDocument();
     expect(player1).toBeInTheDocument();
     expect(player2).toBeInTheDocument();
     expect(playerScore1).toBeInTheDocument();
