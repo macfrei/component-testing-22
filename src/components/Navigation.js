1f8fcfaab0d0fedd382ec809c78a4fa3fe457577
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-16 08:06:55 +0100

    Change navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
     <Nav>
-      <Link to="/play">Play</Link>
-      <Link to="/history">History</Link>
+      <LinkStyled to="/">Play</LinkStyled>
+      <LinkStyled to="/history">History</LinkStyled>
     </Nav>
   );
 }
 
 const Nav = styled.nav`
   display: flex;
+`;
+
+const LinkStyled = styled(NavLink)`
+  background-color: lightgray;
+  border: 1px solid black;
+  width: 100%;
+  padding: 5px;
+  text-align: center;
+  text-decoration: none;
+  color: black;
+
+  &.active {
+    background-color: #333;
+    color: white;
+  }
 `;
