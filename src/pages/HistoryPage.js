91a494786185dd23cdaba4d4a3ccbbcf91d0c386
Marie-Carleen Frei <lene@7freiheiten.de>
2022-02-16 08:07:04 +0100

    Add navigation to page

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 import HistoryEntry from '../components/HistoryEntry';
+import Navigation from '../components/Navigation';
 
 export default function HistoryPage({ history }) {
   return (
-    <HistoryWrapper>
-      <h2>Previous Games</h2>
-      {history.map(({ nameOfGame, players, id }) => (
-        <HistoryEntry key={id} nameOfGame={nameOfGame} players={players} />
-      ))}
-    </HistoryWrapper>
+    <>
+      <HistoryWrapper>
+        <h2>Previous Games</h2>
+        {history.map(({ nameOfGame, players, id }) => (
+          <HistoryEntry key={id} nameOfGame={nameOfGame} players={players} />
+        ))}
+      </HistoryWrapper>
+      <Navigation />
+    </>
   );
 }
 
